496cb177867eaecb32886e935ad1046c340ccafb
Franz Rodriguez <frodriguez@its.com>
2020-10-06 12:47:38 -0400

    Docs agregado

---
+++ docs/precache-manifest.1357f019317ed762a16dc27cd7e6ae64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007c191ac6b0023117b612ce9b0a3230",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c643f1067b074a64a67a",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c643f1067b074a64a67a",
+    "url": "/static/js/main.1812ae9a.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
